f1ec0d30c8a36a5ab0a4b4cee673c77ac2ab7cac
aashishsood <aashish.sood@outlook.com>
2018-02-26 23:15:27 -0800

    Pushed all the code with comments and final.tsv

--- Assignment1/NearDist.py
+++ Assignment1/NearDist.py
@@ -6,6 +6,7 @@ import pickle
 
 from pymongo import MongoClient
 
+# this DB  contains allthe unique location strings and the corresponding longitude and latitudes
 db = MongoClient().ufo.cities
 
 airport_fname = "airport_data.pickle"
@@ -19,26 +20,21 @@ with open(us_neighbor_fname, 'rb') as f:
 print(type(airport_data),len(airport_data))
 print(type(us_neighbour_data),len(us_neighbour_data))
 
+#This function will generate the airport list for each US/Non-US location
 def generateAirportList(region_list):
-    #print("In generateAirportList")
     airport_list = []
     for region in region_list:
         airport_list = airport_list + airport_data[region]
     return airport_list
 
+#This function will iterate over each airport in the airport list and try to find the nearest airport metrics for the city.
 def findNearest(city,region_list):
-    #print("In findNearest",str(city))
     airport_list = generateAirportList(region_list)
-    #print(city,type(city))
     city_lat_lon = (float(city['latitude']),float(city['longitude']))
-    #print(city_lat_lon)
     min_dist = None
     airport_name = None
     for airport in airport_list:
-
-        #print("Checking for airport:- ",airport)
         airport_lat_lon = (airport[1],airport[2])
-        #print(airport_lat_lon)
         vin_dist = vincenty(city_lat_lon, airport_lat_lon).miles
         if min_dist is None:
             min_dist = vin_dist
@@ -49,15 +45,17 @@ def findNearest(city,region_list):
                 airport_name= airport[0]
     return airport_name,min_dist
 
-# sanity checks
-# Iterate over each city to get the nearest airport
-# Assumption:- For the US sightings we assume that the nearest airport will be in
+
+#Iterate over each city to get the nearest airport
+#Assumption:- For the US sightings we assume that the nearest airport will be in eather the same state or neighbouring state
+#For the sightings in non-US  countries, the airport is in t he same country
 count = 0
 
 for row in db.find():
+    #print(count)
     if count%100 == 0:
         print(count)
-    count=count+1
+    #count=count+1
     region = row['region']
     if region == "US":
         # check for all US regions.
@@ -65,13 +63,13 @@ for row in db.find():
         airport_name, dist = findNearest(row, region_list)
     elif region[:2] == "US":
         # This loop is for all the US states
-        region_list = us_neighbour_data[region]
+        region_list = us_neighbour_data[region][:]
         region_list.append(region)
         airport_name,dist = findNearest(row,region_list)
     else:
         # This loop is for Non US regions
         region_list = [region]
         airport_name, dist = findNearest(row, region_list)
-    #print(airport_name,dist)
     count = count+1
+    #Update the DB with nearest airport name and distance
     db.update_one({'_id':row['_id']},{"$set":{'Nearest Airport':airport_name,'Distance':dist}})
--- Assignment1/geo.py
+++ Assignment1/geo.py
@@ -10,8 +10,10 @@ from collections import defaultdict
 from geopy.distance import vincenty
 import pickle
 
-with open('ufo_awesome.json') as json_data:
+with open('ufo_awesome.json',encoding="utf8") as json_data:
     ufo_data = json.load(json_data)
+    print(len(ufo_data))
+    print(ufo_data[0])
 
 us_state_code = open('state_code.csv', "r")
 us_state = csv.reader(us_state_code)
@@ -25,33 +27,34 @@ count = 0
 # US sightings
 # There is a list of neighbouring USA states.
 # from this we can form the region ISO code. this will help narrow down the search of airports.
-#There are 9019 non US sightings.
-#reverse query will help get the country and state of the sighting.
-#all the airports in the list have their corresponding region ISO code.
+# There are 9019 non US sightings.
+# reverse query will help get the country and state of the sighting.
+# all the airports in the list have their corresponding region ISO code.
 # From the country and state we got from the reverse query, we need to form the region ISO code.
-#Need to find a way to generate region ISO code from state and country. GeoName package can help. but need to search more on this.
+# Need to find a way to generate region ISO code from state and country. GeoName package can help. but need to search more on this.
 ufo_region_data=[]
-count=64
+count=0
 exception = False
 geocod_dict = {}
-for i,data in enumerate(ufo_data[4158:]):
+
+for i,data in enumerate(ufo_data):
     exception = False
     if i%10 == 0:
-        print(i)
+        print(i) # print count for every 10th iteration
     if i%300 == 0:
         if i != 0:
             print("Create a new file")
             fname = "ufo_region_data"+str(count)+".pickle"
             count = count+1
+            #Create pickle files for every 300 locationsinthe original UFO sighting JSON
             with open(fname, 'wb') as f:
                 pickle.dump(ufo_region_data, f)
                 ufo_region_data = []
     d={}
     location = data["location"]
-    #print("Working on loc:- ",location)
     try:
         if location not in geocod_dict:
-            geo_location = geolocator.geocode(location,timeout=10)
+            geo_location = geolocator.geocode(location,timeout=10)  # Get the geocode values from the API
             geocod_dict[location] = geo_location
         else:
             geo_location = geocod_dict[location]
@@ -61,8 +64,7 @@ for i,data in enumerate(ufo_data[4158:]):
     if geo_location is not None and exception == False:
         d['lat'],d['lon'] = geo_location.latitude, geo_location.longitude
         split_loc = location.split(",")
-        if split_loc[-1].strip().upper() not in state_set:
-            #print(location)
+        if split_loc[-1].strip().upper() not in state_set: # This check is for Non US states, we need to capture the country/region using the reverse query.
             query_string = str(geo_location.latitude)+", "+str(geo_location.longitude)
             try:
                 reverse_loc = geolocator.reverse(query_string,timeout=10)
@@ -77,7 +79,6 @@ for i,data in enumerate(ufo_data[4158:]):
             d["state"] = split_loc[-1].strip().upper()
             d["region"] = d["country"]+"-"+d["state"]
     ufo_region_data.append(d)
-    #   sleep(1)
+    sleep(1) # Introduced sleep parameter to notoverwhelm thethe geopy API with too many requests
 
-with open(fname, 'wb') as f:
-    pickle.dump(ufo_region_data, f)
+
