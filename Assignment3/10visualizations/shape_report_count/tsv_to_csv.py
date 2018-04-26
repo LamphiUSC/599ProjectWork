5e60320e4c4f171c31905ace65e8f59c76088cf2
Parul Priya <parul71625@guest-wireless-upc-1607-10-120-121-194.usc.edu>
2018-04-26 15:32:18 -0700

    Visualizations updated

---
+++ Assignment3/10visualizations/shape_report_count/tsv_to_csv.py
@@ -0,0 +1,20 @@
+import csv
+
+data={}
+count = 1
+fcsv = csv.writer(open("ShapeReported_year_count.csv", "w"))
+fcsv.writerow(["Year", "OneD", "TwoD", "ThreeDOrObject", "Unknown"])
+with open("ShapeReported_year_count.tsv","r", encoding = "ISO-8859-1") as f:
+    for line in f:
+       if count == 1:
+           count = count + 1
+           continue
+       sp=line.split("\t")
+
+       if len(sp) < 3:
+           continue
+       # print(len(sp))
+       csvList = []
+       for i in range(0,len(sp)):
+           csvList.append(sp[i].strip())
+       fcsv.writerow(csvList)
