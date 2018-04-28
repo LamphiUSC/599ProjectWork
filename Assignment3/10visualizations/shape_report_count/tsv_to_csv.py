490bbd632121eb33c1bc023145998a1d0a77467b
LamphiUSC <chirumam@usc.edu>
2018-04-28 15:22:24 -0700

    corrected heading for shape, modified 1D,2D etc, and report

--- Assignment3/10visualizations/shape_report_count/tsv_to_csv.py
+++ Assignment3/10visualizations/shape_report_count/tsv_to_csv.py
@@ -5,7 +5,7 @@ count = 1
 
 # tsv to csv
 fcsv = csv.writer(open("ShapeReported_year_count.csv", "w"))
-fcsv.writerow(["Year", "OneD", "TwoD", "ThreeDOrObject", "Unknown"])
+fcsv.writerow(["Year", "1D", "2D", "3D or Object", "Unknown"])
 with open("ShapeReported_year_count.tsv","r", encoding = "ISO-8859-1") as f:
     for line in f:
        if count == 1:
